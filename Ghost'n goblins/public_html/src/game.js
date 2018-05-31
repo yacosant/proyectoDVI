56aadbb80242845232dc5825d83a958ce5ff9ad4
Andrea <andreamartin@ucm.es>
2018-05-31 17:21:38 +0200

    EL HUD DE VIDAS NO FUNCIONA!!

--- Ghost'n goblins/public_html/src/game.js
+++ Ghost'n goblins/public_html/src/game.js
@@ -1626,17 +1626,6 @@ Q.UI.Text.extend("Lives",{
     }
 });
 
-Q.UI.Button.extend("LivesPNG",{
-    init:function(p) {
-        this._super({
-            asset: "1up.png",     
-            x: 150,
-            y: 150
-            });
-        
-    }
-});
-
 //Temporizador
 Q.UI.Text.extend("Timer",{
     init:function(p) {
@@ -1664,13 +1653,12 @@ Q.scene('HUD',function(stage) {
   var container = stage.insert(new Q.UI.Container({x:0, y: 1, fill: "rgba(0,0,0,1)"}));
   container.insert(new Q.Score());
   container.insert(new Q.Lives());
-  var vidas = Q.state.get("lives");
+  
   var x = 150;
-  for(var i = 0; i < vidas; i++){
-    container.insert(new Q.LivesPNG({x: x,y: 150}));
-    x += 10;
+  for(var i = 0; i < 3; i++){
+    container.insert(new Q.LivesPNG({x: x,y: 500}));
+    x = x + 30;
   }
-  container.insert(new Q.LivesPNG());
   container.insert(new Q.Timer());
   container.fit(5,200);
   stage.show= function(state){
