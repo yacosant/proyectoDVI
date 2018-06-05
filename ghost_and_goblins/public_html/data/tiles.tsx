13c593602c79db3e04c3346968297bcdde599b0d
Jose Luis <joseluissanchez11@gmail.com>
2018-06-05 17:59:22 +0200

    Limpieza

--- ghost_and_goblins/public_html/data/tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tiles" tilewidth="32" tileheight="32" tilecount="130" columns="10">
- <image source="../images/tiles.png" width="320" height="416"/>
-</tileset>
